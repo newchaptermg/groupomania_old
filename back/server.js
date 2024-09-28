c15dcac7ac96b41b8add545ff87acdd3101fc495
MG <mohankumar.g31@gmail.com>
2024-09-28 09:51:27 -0400

    Fixed the Front and back end integeration using CORS

--- back/server.js
+++ back/server.js
@@ -1,20 +1,22 @@
 require('dotenv').config();
-
 const express = require('express');
+const cors = require('cors');  // Import the CORS middleware
 const authRoutes = require('./routes/auth');
 const postRoutes = require('./routes/posts');
 const errorHandler = require('./middleware/errorHandler'); // Import the error handler
 
-
 const app = express();
+
+// Enable CORS
+app.use(cors());
+
+// Middleware to parse JSON
 app.use(express.json());
-
-
 
 // Define the root route
 app.get('/', (req, res) => {
-    res.send('Welcome to the Groupomania API');
-  });
+  res.send('Welcome to the Groupomania API');
+});
 
 // Routes
 app.use('/auth', authRoutes);
